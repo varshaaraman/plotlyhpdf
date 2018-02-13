4ae7332a6e18c893c1c6f60cc856cdbb2805d71d
Varshit07 <varshitshah97@gmail.com>
2018-02-13 15:53:09 +0530

    adding a sample ui for group task

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -1,21 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import xhr from 'xhr';
+import Plot from './Plot';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  state = {
+    location: '',
+  data: {},
+  dates: [],
+  temps: []
   }
-}
+  fetchData = (evt) => {
+    evt.preventDefault();
 
+    var location = encodeURIComponent(this.state.location);
+
+    var urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+    var urlSuffix = '&APPID=d231efc52bef00d653e1d966b0840736&units=metric';
+    var url = urlPrefix + location + urlSuffix;
+
+    var self = this;
+
+    xhr({
+      url: url
+    }, function (err, data) {
+      var body = JSON.parse(data.body);
+           var list = body.list;
+           var dates = [];
+           var temps = [];
+           for (var i = 0; i < list.length; i++) {
+             dates.push(list[i].dt_txt);
+             temps.push(list[i].main.temp);
+           }
+
+           self.setState({
+             data: body,
+             dates: dates,
+             temps: temps
+           });
+         });
+       };
+
+
+changeLocation = (evt) => {
+  this.setState({
+    location: evt.target.value
+  });
+};
+render() {
+  var currentTemp = 'not loaded yet';
+  if (this.state.data.list) {
+    currentTemp = this.state.data.list[0].main.temp;
+  }
+  return (
+    <div>
+      <h1>Weather</h1>
+      <form onSubmit={this.fetchData}>
+        <label>I want to know the weather for
+          <input
+            placeholder={"City, Country"}
+            type="text"
+            value={this.state.location}
+            onChange={this.changeLocation}
+          />
+        </label>
+      </form>
+      {/*
+        Render the current temperature and the forecast if we have data
+        otherwise return null
+      */}
+      {(this.state.data.list) ? (
+        <div className="wrapper">
+          <p className="temp-wrapper">
+            <span className="temp">{ currentTemp }</span>
+            <span className="temp-symbol">°C</span>
+          </p>
+          <h2>Forecast</h2>
+          <Plot
+            xData={this.state.dates}
+            yData={this.state.temps}
+            type="scatter"
+          />
+        </div>
+      ) : null}
+
+    </div>
+  );
+}
+}
 export default App;
